0eea315f62b9038bd0da57f4079a237d427e8d83
Yurii Antonchyk <xsat.php@gmail.com>
2022-11-04 17:39:24 +0200

    RAS and Hexadecimal keys

--- js/main.js
+++ js/main.js
@@ -1,27 +1,34 @@
-const ELEMENTS = ['A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+function getRandElement(elements) {
+	const index = Math.floor(Math.random() * elements.length);
 
-function getRandElement() {
-	const index = Math.floor(Math.random() * ELEMENTS.length);
-
-	return ELEMENTS[index];
+	return elements[index];
 }
 
-function getRandHash(size) {
-  let hash = '';
+function getRandHash(size, elements) {
+	let hash = '';
 
-  for (let i = 0; i < size; i++) {
-    hash += getRandElement();
-  }
+	for (let i = 0; i < size; i++) {
+	hash += getRandElement(elements);
+	}
+
+	return hash;
+}
+
+function generateHexadecimalKey(size_input_id) {
+	const size = document.getElementById(size_input_id).value;
+	const elements = ['A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   
-  return hash;
+	alert(getRandHash(size, elements));
 }
 
-function generateRandHash() {
-	const size = document.getElementById('size').value;
+function generateRASID(size_input_id) {
+	const size = document.getElementById(size_input_id).value;
+	const elements = [
+		'-', '_', '$', '#', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 
+		'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 
+		'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 
+		'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+	];
   
-  alert(getRandHash(size));
-}
-
-function presetSize(value) {
-	document.getElementById('size').value = value;
+	alert(getRandHash(size, elements));
 }
